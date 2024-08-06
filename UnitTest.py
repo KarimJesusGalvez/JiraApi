2e773d41247e0f162683c0e81d2d0a662b225eaf
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2024-08-06 11:03:23 +0200

    Added, get xray test status definition unit test

--- UnitTest.py
+++ UnitTest.py
@@ -1,15 +1,16 @@
 import importlib
 from unittest.mock import Mock
-
 from _pytest.fixtures import fixture
-
 from Config.Auth.Token import Token_Headers
 from Config.Auth.Token.Token_Headers import read_token, get_headers
+from XrayApi.Tests import get_defined_test_status
+
 
 @fixture
-def reload():
+def reload_headers():
     yield
     importlib.reload(Token_Headers)
+
 
 class TestAuth:
 
@@ -18,8 +19,16 @@ class TestAuth:
         if len(result) != 44:
             raise AssertionError("Token length")
 
-    def test_get_headers(self, reload):
+    def test_get_headers(self, reload_headers):
         result = get_headers()
         Token_Headers.read_token = Mock(return_value="AA")
         assert "Authorization" in result
-        assert "Accept" in  result
+        assert "Accept" in result
+
+
+class TestXray:
+
+    class TestTestIssues:
+
+        def test_get_defined_test_status(self):
+            assert len(get_defined_test_status().parsed) > 1
