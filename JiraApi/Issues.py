f7786b4cd655bb9f62643999d774447c098ff4ef
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2024-08-07 13:27:30 +0200

    Minor refactor, execute_query name

--- JiraApi/Issues.py
+++ JiraApi/Issues.py
@@ -5,14 +5,14 @@ from jira import JIRA, Issue
 log = logging.getLogger("Jira.Issues")
 
 
-def execute_query(jira: JIRA, query: str) -> list[Issue]:
+def search_issues(jira_server: JIRA, query: str) -> list[Issue]:
     query = escape_reserved_words_in_query(query)
     log.info(f"Executing query: {query} ...\n")
-    existing_issues = jira.search_issues(query, maxResults=2000)
+    existing_issues = jira_server.search_issues(query, maxResults=2000)
 
     issues_found = ""
     for issue in existing_issues:
-        issues_found += f'\n{issue} {jira.issue(issue).fields.summary}'
+        issues_found += f'\n{issue} {jira_server.issue(issue).fields.summary}'
     if issues_found:
         log.info(f"Found {int(len(existing_issues))} issue/s:" + issues_found)
     else:
@@ -31,8 +31,10 @@ def escape_reserved_words_in_query(query: str) -> str:
     return query
 
 
-def check_existing_issue(jira: JIRA, project_id: int, issue_type: str, summary: str) -> list | None:
-    issues = execute_query(jira, f'project = {project_id} AND type = "{issue_type}" AND summary ~ "\\"{summary}\\""')
+def check_existing_issue(jira_server: JIRA, project_id: int, issue_type: str, summary: str) -> list | None:
+    issues = search_issues(jira_server, f'project = {project_id} '
+                                        f'AND type = "{issue_type}" '
+                                        f'AND summary ~ "\\"{summary}\\""')
     if len(issues) > 0:
         log.warning(f"{len(issues)} Issue/s found for {summary}, {issues[0].key}")
     else:
@@ -40,23 +42,23 @@ def check_existing_issue(jira: JIRA, project_id: int, issue_type: str, summary: 
     return issues
 
 
-def update_link(jira: JIRA, link_type: str, in_query: str, out_query: str) -> None:
-    if not check_link_name(jira, link_type):
+def update_link(jira_server: JIRA, link_type: str, in_query: str, out_query: str) -> None:
+    if not check_link_name(jira_server, link_type):
         raise ValueError(f"Invalid link {link_type}")
 
-    in_issues = execute_query(jira, in_query)
-    out_issues = execute_query(jira, out_query)
+    in_issues = search_issues(jira_server, in_query)
+    out_issues = search_issues(jira_server, out_query)
     for in_issue in in_issues:  # if issues > 1 version is equal
         for out_issue in out_issues:  # if issues > 1 version is equal
             log.info(f"Updating link; {str(in_issue.fields.issuetype)} "
                      f"{in_issue.key} -> "
                      f"{link_type} -> "
                      f"{str(out_issue.fields.issuetype)} {out_issue.key} ...")
-            jira.create_issue_link(link_type, in_issue.key, out_issue.key)
+            jira_server.create_issue_link(link_type, in_issue.key, out_issue.key)
 
 
-def check_link_name(jira: JIRA, link_type: str) -> bool:
-    valid_names = jira.issue_link_types()
+def check_link_name(jira_server: JIRA, link_type: str) -> bool:
+    valid_names = jira_server.issue_link_types()
     if link_type in valid_names:
         log.debug(f"Link {link_type} is valid")
         return True
@@ -65,16 +67,16 @@ def check_link_name(jira: JIRA, link_type: str) -> bool:
         return False
 
 
-def create_issue_if_not_existing(jira: JIRA, projectid: int, issue_dict: dict) -> Issue | None:
-    if not check_existing_issue(jira, projectid, "Test", issue_dict["Summary"]):
-        return create_issue(jira, issue_dict)
+def create_issue_if_not_existing(jira_server: JIRA, projectid: int, issue_dict: dict) -> Issue | None:
+    if not check_existing_issue(jira_server, projectid, "Test", issue_dict["Summary"]):
+        return create_issue(jira_server, issue_dict)
     else:
         log.warning("Issue/s found, Skipping Issue creation")
 
 
-def create_issue(jira: JIRA, issue_dict: dict) -> Issue | None:
+def create_issue(jira_server: JIRA, issue_dict: dict) -> Issue | None:
 
-    new_issue = jira.create_issue(fields=issue_dict)
+    new_issue = jira_server.create_issue(fields=issue_dict)
     log.warning(f"Created new {new_issue.fields.issuetype} issue: {new_issue.key}")
     return new_issue
 
