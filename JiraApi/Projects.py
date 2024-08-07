25e5016fbdc153e9ff2a67606336cd864eec6868
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2024-08-07 13:38:55 +0200

    Minor refactor, changed project data to dict

--- JiraApi/Projects.py
+++ JiraApi/Projects.py
@@ -4,11 +4,11 @@ from jira import JIRA
 log = logging.getLogger("Jira.Projects")
 
 
-def get_project_data(jira: JIRA, project_id: str) -> list[str, str, str]:
+def get_project_data(jira: JIRA, project_id: str) -> dict[str, str]:
     """project_id: project id, name or key"""
     for project in jira.projects():
-        ids = [project.raw["name"], project.raw["key"], project.raw["id"]]
-        if project_id in ids:
+        ids = {"name": project.raw["name"], "key": project.raw["key"], "id": project.raw["id"]}
+        if project_id in ids.values():
             log.debug(f"Found project {ids}")
             return ids
     msg = f"Project {project_id} not found in list {jira.projects()}"
