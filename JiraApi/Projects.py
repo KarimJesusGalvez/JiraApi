8325763f8d034657d847d04d03a3873345381918
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2024-08-08 09:15:33 +0200

    Added Jira get field type

--- JiraApi/Projects.py
+++ JiraApi/Projects.py
@@ -26,8 +26,59 @@ def get_project_issue_types(jira_server: JIRA, project_id: str) -> dict[str, str
     log.debug(f"Retrieved issue types for project {project_id}, {issue_types}")
     return issue_types
 
-def _parse_meta_issue_fields_type(response):
-    raise NotImplementedError("")
+
+def _parse_meta_issue_fields_name(response: dict):
+    result = {}
+    for data in response["values"]:
+        if data["name"] == "Issue Type":
+            continue
+        result[data["name"]] = data
+    return result
+
+
+def _print_meta_issue_dict(field: dict, depth: int) -> str:
+    sep = "    " * depth
+    msg = "\n"
+    for key, value in field.items():
+        if isinstance(value, dict):
+            msg += _print_meta_issue_dict(value, depth + 1)
+        else:
+            msg += f"{sep}{key}, {value}"
+    return msg
+
+
+def _parse_meta_issue_fields_data_type(field: dict) -> dict:
+    return field["schema"]["type"]
+
+
+def _print_meta_issue_fields_data(field: dict) -> str:
+    msg = ""
+    sep = "    "
+
+    for key, value in field.items():
+        if isinstance(value, dict):
+            _print_meta_issue_dict(value, 1)
+        elif isinstance(value, list):
+            for val in value:
+
+                msg = f"{sep}[{key}"
+                if isinstance(value, dict):
+                    msg += _print_meta_issue_dict(value, 2)
+                else:
+                    msg += f"\n{sep * 2}{val}"
+                msg += f"\n{sep}]"
+        else:
+            msg += f"{sep}{key}, {value}"
+    return msg
+
+
+def _parse_meta_issue_fields_type(response: dict):
+    result = {}
+    for field, data in _parse_meta_issue_fields_name(response).items():
+        log.debug(f"Parsing field {field}")
+        log.debug(_print_meta_issue_fields_data(data))
+        result[field] = _parse_meta_issue_fields_data_type(data)
+    return result
 
 
 def get_fields_for_all_issue_types(jira_server: JIRA, project_id: str) -> dict:
@@ -41,7 +92,7 @@ def get_fields_for_all_issue_types(jira_server: JIRA, project_id: str) -> dict:
                                             f"\n Headers:{response.raw.headers}" \
                                             f"\n Body:{response.raw._body}"
         result[issue_type['name']] = _parse_meta_issue_fields_type(response.json())
-    print(result)
+        log.info(f"Issue type {issue_type['name']} has fields {result[issue_type['name']]}")
     return result
 
 
